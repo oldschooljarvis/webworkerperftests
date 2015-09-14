1cb1841c58f0b14610f969356838acae6aa2978c
Edwin Jarvis <oldschooljarvis@users.noreply.github.com>
2015-09-14 01:34:24 -0500

    Firefox-optimized version. Drastically better results.

---
+++ Test05-optimized/index.js
@@ -0,0 +1,21 @@
+var t = performance.now();
+console.log('UI load timestamp: ' + t);
+
+var worker1 = new Worker('worker1.js');
+
+worker1.onmessage = function(e) {
+    //Receive and log.
+    t = performance.now();
+    console.log('Worker --> UI: ' + (t - e.data));
+}
+
+function rafLoop() {
+    //Send a message, and receive a reply (once per-frame).
+    setTimeout(function() {
+        t = performance.now();
+        worker1.postMessage(t);
+    }, 0);
+
+    window.requestAnimationFrame(rafLoop);
+}
+rafLoop();
